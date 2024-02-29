3a60218815fd08778a4b543b7d0c7c6de323a092
ak4m410x01 <ak4m410x01@gmail.com>
2024-02-29 21:14:23 +0200

    remove storing file

--- LoginsManager/obj/Debug/net8.0/LoginsManager.AssemblyInfo.cs
+++ LoginsManager/obj/Debug/net8.0/LoginsManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginsManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8660597eef15b6f6cf2b353840e02d23e8372ec7")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginsManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginsManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
